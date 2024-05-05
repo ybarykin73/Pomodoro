8c132a38836fb7b9560629c3980502f775e50c52
YuraBarykin <yBarykin@gmail.com>
2024-05-05 09:30:00 +0400

    Добавлен sass плагин

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/app-component/App'
 
-import './index.css'
+import './index.scss'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(png|jpg|jpef|gif)$/i,
