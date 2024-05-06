7e3173cfed412e3efe4ab104cc5d6224529e1b45
YuraBarykin <yBarykin@gmail.com>
2024-05-06 15:24:10 +0400

    Добавлен компонент таймера

---
+++ src/components/time/Time.jsx
@@ -0,0 +1,15 @@
+import './Time.scss'
+
+const Time = () => {
+  return (
+    <div>
+      <div className="time">
+        15:30
+      </div>
+      <h2>Концентрация</h2>
+    </div>
+
+  )
+}
+
+export default Time
