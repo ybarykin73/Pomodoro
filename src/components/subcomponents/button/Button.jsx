11fad67cebefa765fbf047e1dcf52d1603da937e
YuraBarykin <yBarykin@gmail.com>
2024-05-06 15:22:42 +0400

    Добавлен компонент кнопки

---
+++ src/components/subcomponents/button/Button.jsx
@@ -0,0 +1,20 @@
+import './Button.scss'
+
+const Button = (props) => {
+
+  const {
+    onClick,
+    children
+  } = props
+
+  return (
+    <button
+      className='button'
+      onClick={onClick}
+    >
+      { children }
+    </button>
+  )
+}
+
+export default Button
